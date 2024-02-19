642effa40ccc05b1af1e5d8d332d7331d241c5a1
Ben04051 <benmitchell0405@hotmail.co.uk>
2024-02-19 14:02:18 +0000

    update port

--- Game-app-backend/connection.js
+++ Game-app-backend/connection.js
@@ -50,7 +50,7 @@ app.get("/leaderboard", (req, res) => {
 
 module.exports= app
 
-const PORT = process.env.PORT || 3000; // Use the provided port by Render or default to 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
